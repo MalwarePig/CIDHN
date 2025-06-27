9457b98bb8a6e9442c07f70641f3a19151301d6b
Malwarepig <sergio_lcc92@hotmail.com>
2025-06-26 18:00:46 -0600

    diseño completo, falta optimizacion

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -24,11 +24,11 @@
     });
 }); */
 
-
-document.addEventListener('DOMContentLoaded', function() {
+/* 
+document.addEventListener('DOMContentLoaded', function () {
   const header = document.querySelector('header');
   const section = document.querySelector('section'); // O el section específico
- const LogoHeader = document.getElementById('LogoHeader');
+  const LogoHeader = document.getElementById('LogoHeader');
 
   let empalmado = false;
   let esperando = false;
@@ -43,24 +43,52 @@ document.addEventListener('DOMContentLoaded', function() {
     if (!empalmado && diferencia > umbral) {
       empalmado = true;
       header.classList.add('oculto'); // Oculta el header cuando #Inicio está visible
-            LogoHeader.classList.add('oculto'); 
+      LogoHeader.classList.add('oculto');
       console.log('El header se empalma con el section');
-    } 
+    }
     // Detecta NO empalme si el header está por encima del section
     else if (empalmado && headerRect.bottom <= sectionRect.top) {
       empalmado = false;
-      header.classList.remove('oculto');  
-            LogoHeader.classList.remove('oculto');
+      header.classList.remove('oculto');
+      LogoHeader.classList.remove('oculto');
       console.log('El header ya no se empalma con el section');
     }
     // Si está entre umbrales, no cambia el estado
   }
 
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', function () {
     if (!esperando) {
       esperando = true;
       checkEmpalme();
       setTimeout(() => esperando = false, 100); // throttle de 100ms
     }
   });
+}); */
+
+function showSideBar() {
+  document.querySelector('.menu-responsive').style.display = 'flex';
+  document.querySelector('.menu-button').style.display = 'none';
+}
+
+function hideSideBar() {
+  document.querySelector('.menu-responsive').style.display = 'none';
+  document.querySelector('.menu-button').style.display = 'flex';
+}
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+    const inicio = document.getElementById('portada');
+    const buttonHome = document.getElementById('up')
+
+    const observer = new IntersectionObserver(
+        ([entry]) => {
+            if (entry.isIntersecting) { 
+                buttonHome.classList.add('oculto'); // Oculta el header cuando #Inicio está visible
+            } else { 
+                buttonHome.classList.remove('oculto'); // Oculta el header cuando #Inicio está visible
+            }
+        },
+        { threshold: 0.5 } // Puedes ajustar el umbral según lo que necesites
+    );
+    if (inicio) observer.observe(inicio);
 });
